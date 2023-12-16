7a40a079f2c7cca5bb6e460d592f9bbbc7a1f8eb
Shivani Sehgal <sehgalshivani07@gmail.com>
2023-12-16 10:08:03 -0500

    added authentication check on navigation

--- js/events.js
+++ js/events.js
@@ -1,14 +1,23 @@
+import { isAuthenticated } from "./module.mjs";
+
 /**
  * Adding dark theme css on window load based on darkMode slider input
  */
 window.addEventListener("load", function() {
     // Removing bg color transition so that it doesn't happens on every page load
     removeTransition();
-    if (window.localStorage.getItem("darkMode") == "true") {
-        document.getElementById("dark-mode").checked = true;
-        addDarkTheme();
-    } else {
-        document.getElementById("dark-mode").checked = false;
-        removeDarkTheme();
+    let darkModeSlider = document.getElementById("dark-mode");
+    if (darkModeSlider) {
+        if (window.localStorage.getItem("darkMode") == "true") {
+            darkModeSlider.checked = true;
+            addDarkTheme();
+        } else {
+            darkModeSlider.checked = false;
+            removeDarkTheme();
+        }
     }
-})
+});
+
+navigation.addEventListener("navigate", () => {
+    isAuthenticated();
+});
