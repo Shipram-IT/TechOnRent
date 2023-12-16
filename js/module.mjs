b047b66d36cad13c92d4df746a47a7896d44a463
Shivani Sehgal <sehgalshivani07@gmail.com>
2023-12-16 16:21:04 -0500

    Fixed minor design issues

--- js/module.mjs
+++ js/module.mjs
@@ -2,7 +2,7 @@ export function isAuthenticated() {
     let prePath = "./";
     let isAuth = JSON.parse(localStorage.getItem("isAuthenticated"));
     if (!isAuth) {
-        if (window.location.pathname == "/index.html") prePath ="./html/";
+        if (window.location.pathname == "/index.html" || window.location.pathname == '/') prePath ="./html/";
         window.location = prePath + "login.html";
     }
 }
